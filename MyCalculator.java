ab5dc142532ea4607fb02223fd6e3753173f9a3a
AldoRiz <aldoruiz342@hotmail.com>
2018-11-20 11:05:19 +0100

    tja tjena ni vet vad

--- MyCalculator.java
+++ MyCalculator.java
@@ -2,43 +2,51 @@ import java.util.Scanner;
 
 public class MyCalculator {
 
-  private static double num1 = 0;
-  private static double num2 = 0;
-  private static double answer = 0;
-  private static String operation = "-1";
+  private double num1;
+  private double num2;
+  private double answer;
+  private String operation = "-1";
+
+  Scanner op = new Scanner (System.in);
 
   Scanner scan = new Scanner(System.in);
 
-    private static void add(double num1, double num2){
-      answer = num1 + num2;
-    }
-    private static void sub(double num1, double num2){
-      answer = num1 - num2;
-    }
-    private static void mult(double num1, double num2){
-      answer = num1 * num2;
-    }
-    private static void div(double num1, double num2){
-      answer = num1 / num2;
-    }
-    private static void enterDouble(){
-      num1 = scan.nextDouble();
-    }
-    private static void enterOperator(){
-      operation = op.next();
-    }
-    private static void enterDoubleAgain(){
-      num2 = scan.nextDouble();
-    }
+      private  void printInstructions(){
 
-    private static void printResult(){
-      System.out.println("Your answer is: " + answer);
-    }
-    private static void printInstructions(){
       System.out.println("Hello, welcome to MasterGitCalculator");
       System.out.println("please choose number, operator, number");
 
-    }
+
+
+
+          num1 = scan.nextDouble();
+
+          operation = op.next();
+
+          num2 = scan.nextDouble();
+
+      if (operation == "+");
+      {
+          System.out.println("ditt svar ar" + (num1 + num2));
+      }
+      if  (operation == "-");
+      {
+          System.out.println("ditt svar ar" + (num1 - num2));
+      }
+
+
+      if(operation == "/");
+      {
+          System.out.println("ditt svar ar" + (num1 / num2));
+      }
+      if (operation == "*")
+      {
+          System.out.println("ditt svar ar" + (num1 * num2));
+      }
+
+
+
+ }
 
 
   public static void main(String[] args) {
@@ -48,10 +56,7 @@ public class MyCalculator {
     MyCalculator myC = new MyCalculator();
 
     myC.printInstructions();
-    myC.enterDouble();
-    myC.enterOperator();
-    myC.enterDoubleAgain();
-    myC.printResult();
+
 
 
 
