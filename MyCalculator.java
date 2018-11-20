5b093d39926cdf274e95bbf53fcef5a4a33a43c1
AldoRiz <aldoruiz342@hotmail.com>
2018-11-20 15:08:07 +0100

    try

--- MyCalculator.java
+++ MyCalculator.java
@@ -1,66 +1,54 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class MyCalculator {
 
   private double num1;
   private double num2;
   private double answer;
-
   private String operation = "-1";
 
   Scanner op = new Scanner (System.in);
-
   Scanner scan = new Scanner(System.in);
 
-      private  void printInstructions(){
 
-
+  private  void printInstructions(){
+try{
       System.out.println("Hello, welcome to MasterGitCalculator");
       System.out.println("please choose number, operator, number");
 
-
-
-
-          num1 = scan.nextDouble();
-
-          operation = op.next();
-
-          num2 = scan.nextDouble();
+      num1 = scan.nextDouble();
+      operation = op.next();
+      num2 = scan.nextDouble();
 
       if (operation == "+");
       {
-          System.out.println("ditt svar ar" + (num1 + num2));
+          System.out.println("Answer is" + (num1 + num2));
       }
       if  (operation == "-");
       {
-          System.out.println("ditt svar ar" + (num1 - num2));
+          System.out.println("Answer is" + (num1 - num2));
       }
-
-
       if(operation == "/");
       {
-          System.out.println("ditt svar ar" + (num1 / num2));
+          System.out.println("Answer is" + (num1 / num2));
       }
       if (operation == "*")
       {
-          System.out.println("ditt svar ar" + (num1 * num2));
+          System.out.println("Answer is" + (num1 * num2));
       }
-
-
 
  }
 
+catch (Exception e){
+  System.out.println("Error has occured, you messed up!");}
 
-  public static void main(String[] args) {
+
+}
+public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-
     MyCalculator myC = new MyCalculator();
-
     myC.printInstructions();
-
-
-
 
   }
 }
